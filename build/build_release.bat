6d5b882d4c24b7a223bd469c63706380b07da0f5
张鹏 <zhangpeng@renruihr.com>
2026-05-21 09:50:29 +0800

    完善编译脚本

--- build/build_release.bat
+++ build/build_release.bat
@@ -22,3 +22,52 @@ echo Building SQLyog Community x64 Release...
 %MSBUILD% SQLyogCommunity.sln /p:Configuration=Release /p:Platform=x64 /t:Build /m:1 /nodeReuse:false /v:minimal > build_full_log.txt 2>&1
 type build_full_log.txt
 echo Exit code: %ERRORLEVEL%
+
+:: ========== Post-build: copy DLLs and create sqlyog.ini ==========
+echo.
+echo ========================================
+echo Copying runtime DLLs to output...
+echo ========================================
+
+set "OUT_DIR=..\bin\x64\Release"
+set "DLL_SRC=..\lib\x64\release"
+
+:: MySQL connector / SSL / crypto DLLs
+copy /Y "%DLL_SRC%\libcrypto-3-x64.dll"    "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\libssl-3-x64.dll"      "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\libeay32.dll"           "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\ssleay32.dll"           "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\msvcr120.dll"           "%OUT_DIR%\" >nul
+
+:: MySQL authentication plugins
+copy /Y "%DLL_SRC%\caching_sha2_password.dll"  "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\client_ed25519.dll"        "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\sha256_password.dll"       "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\auth_gssapi_client.dll"    "%OUT_DIR%\" >nul
+
+:: MySQL pipe/shmem virtual IO
+copy /Y "%DLL_SRC%\pvio_npipe.dll"          "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\pvio_shmem.dll"          "%OUT_DIR%\" >nul
+
+:: MySQL dialog / Scintilla / HTMLayout
+copy /Y "%DLL_SRC%\dialog.dll"               "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\SciLexer.dll"             "%OUT_DIR%\" >nul
+copy /Y "%DLL_SRC%\htmlayout.dll"             "%OUT_DIR%\" >nul
+
+:: PCRE (from localization)
+copy /Y "..\localization\Tools\libpcre-0.dll" "%OUT_DIR%\" >nul
+
+:: Keywords database (SQL syntax highlighting)
+copy /Y "..\lib\Keywords.db" "%OUT_DIR%\" >nul
+
+:: Create empty sqlyog.ini
+echo. > "%OUT_DIR%\sqlyog.ini"
+
+echo.
+echo ========================================
+echo DLLs and sqlyog.ini ready in:
+echo   %OUT_DIR%
+echo ========================================
+dir /b "%OUT_DIR%\*.dll" | find /c /v ""
+echo DLL files copied.
+dir "%OUT_DIR%\sqlyog.ini"
